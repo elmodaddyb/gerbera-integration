2ba4fbbd85bdef7ae5d8644290ef8dd1d960a5c8
Eamonn <eamonn@buss.us>
2018-11-22 19:39:57 -0500

    Fix permissions on script

--- ui-test/wait-for-grid.sh
+++ ui-test/wait-for-grid.sh
@@ -5,7 +5,9 @@ set -e
 
 cmd="$@"
 
-while ! curl -sSL "http://$HUB_HOST:$HUB_PORT/wd/hub/status" 2>&1 \
+HUB_URL=http://$HUB_HOST:$HUB_PORT/wd/hub/status
+
+while ! curl -sSL "$HUB_URL" 2>&1 \
         | jq -r '.value.ready' 2>&1 | grep "true" >/dev/null; do
     echo 'Waiting for the Grid'
     sleep 1
