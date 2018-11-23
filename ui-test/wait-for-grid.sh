14bce244b1d98d96bedee629bfb24ab528ee4790
Eamonn <eamonn@buss.us>
2018-11-22 19:44:26 -0500

    Add output for hub check

--- ui-test/wait-for-grid.sh
+++ ui-test/wait-for-grid.sh
@@ -9,7 +9,7 @@ HUB_URL=http://$HUB_HOST:$HUB_PORT/wd/hub/status
 
 while ! curl -sSL "$HUB_URL" 2>&1 \
         | jq -r '.value.ready' 2>&1 | grep "true" >/dev/null; do
-    echo 'Waiting for the Grid'
+    echo "Waiting for the Grid: $HUB_URL"
     sleep 1
 done
 
