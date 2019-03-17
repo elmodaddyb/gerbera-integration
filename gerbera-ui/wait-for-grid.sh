61d6deb91e65f9312edc658a14107decfa917902
Eamonn <eamonn@buss.us>
2019-03-17 17:42:07 -0400

    Remove functions from script

--- gerbera-ui/wait-for-grid.sh
+++ gerbera-ui/wait-for-grid.sh
@@ -4,23 +4,16 @@
 # always wait for the grid before launching the test suite for the UI
 
 set -e
-
-function replaceVars () {
-    ${cmd/\$UI_TEST_CMD/$UI_TEST_CMD}
-}
-
-function waitForHub () {
-    HUB_URL=http://$HUB_HOST:$HUB_PORT/wd/hub/status
-
-    while ! curl -sSL "$HUB_URL" 2>&1 \
-            | jq -r '.value.ready' 2>&1 | grep "true" >/dev/null; do
-        echo "Waiting for the Grid: $HUB_URL"
-        sleep 1
-    done
-    >&2 echo "Selenium Grid is up - executing tests"
-}
+HUB_URL=http://$HUB_HOST:$HUB_PORT/wd/hub/status
 
 cmd="$@"
-replaceVars
-waitForHub
+${cmd/\$UI_TEST_CMD/$UI_TEST_CMD}
+
+while ! curl -sSL "$HUB_URL" 2>&1 \
+        | jq -r '.value.ready' 2>&1 | grep "true" >/dev/null; do
+    echo "Waiting for the Grid: $HUB_URL"
+    sleep 1
+done
+>&2 echo "Selenium Grid is up - executing tests"
+
 exec $cmd
