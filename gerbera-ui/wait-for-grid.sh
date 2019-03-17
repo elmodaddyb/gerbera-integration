7715f3a7ccdafac440a5f5d49ab736eac58049db
Eamonn <eamonn@buss.us>
2019-03-17 17:57:28 -0400

    Add comments for clarity

--- gerbera-ui/wait-for-grid.sh
+++ gerbera-ui/wait-for-grid.sh
@@ -6,10 +6,12 @@
 set -e
 HUB_URL=http://$HUB_HOST:$HUB_PORT/wd/hub/status
 
+## hydrate CMD from Dockerfile with ENV
 cmd="$@"
 cmd=${cmd/\$UI_TEST_CMD/$UI_TEST_CMD}
 echo "Command to RUN -----> $cmd"
 
+## wait for grid
 while ! curl -sSL "$HUB_URL" 2>&1 \
         | jq -r '.value.ready' 2>&1 | grep "true" >/dev/null; do
     echo "Waiting for the Grid: $HUB_URL"
@@ -17,4 +19,5 @@ while ! curl -sSL "$HUB_URL" 2>&1 \
 done
 >&2 echo "Selenium Grid is up - executing tests"
 
+## execute the CMD from Dockerfile
 exec $cmd
